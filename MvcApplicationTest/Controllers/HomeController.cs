eb430c2a5566c3f4787aef0c54816b827dc3f008
Nikita <hiresmart@BASEIMAGEDOTNET>
2017-02-08 05:10:23 +0000

    testing

--- MvcApplicationTest/Controllers/HomeController.cs
+++ MvcApplicationTest/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace MvcApplicationTest.Controllers
     {
         public ActionResult Index()
         {
+            //added during demo 8th feb
             return View();
         }
     }
